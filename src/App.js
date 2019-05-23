707253d70722735017b25596153e4f3f817bffd2
Thierry Joux <tjoux@hotmail.com>
2019-05-23 16:49:57 -0700

    Setup redirect path from root and styled further single rest page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from './components/Login'
 import Restaurants from './components/Restaurants';
 import Restaurant from './components/Restaurant';
 import NavBar from './components/NavBar'
-import { withRouter } from 'react-router-dom';
+import { withRouter, Redirect } from 'react-router-dom';
 import {connect} from 'react-redux';
 
 
@@ -25,10 +25,19 @@ class App extends React.Component {
           <NavBar />
         </header>
   
-  
+
         <PrivateRoute exact path='/restaurants' component={Restaurants} />
         <PrivateRoute exact path='/restaurants/:id' component={Restaurant} />
         <Route path='/login' component={Login}/>
+        <Route exact path="/" render={() => (
+          localStorage.getItem('token') ? (
+            <Redirect to="/restaurants" />
+          ) : (
+            <Redirect to="/login" />
+            )
+        )} />
+
+
   
         
       </div>
