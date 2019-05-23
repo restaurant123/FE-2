80b149e5f9cd2ac6c5589a45ed8eff710727ea73
Thierry Joux <tjoux@hotmail.com>
2019-05-23 13:26:33 -0700

    Added new Fields in the Single Restaurant Page

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {incrementVisits, decrementVisits } from '../actions/restaurantsAction';
-import {getRestaurantAction} from '../actions/restaurantAction';
+import { connect } from 'react-redux';
+import { incrementVisits, decrementVisits } from '../actions/restaurantsAction';
+import { getRestaurantAction } from '../actions/restaurantAction';
 import { withRouter } from 'react-router-dom';
 
 import { Container, Row, Col } from 'reactstrap';
@@ -15,7 +15,7 @@ class Restaurant extends React.Component {
 
     componentDidMount() {
         let id = parseInt(this.props.match.params.id, 10);
-        this.props.getRestaurantAction(id)
+        //this.props.getRestaurantAction(id)
     }
 
     handleClickMinus = (id) => {
@@ -34,17 +34,17 @@ class Restaurant extends React.Component {
     render() {
 
         return (
-            
+
             <div className='single-restaurant'>
-                {!this.props.restaurant && 
-                <p>Fetching this restaurant data...</p>}
+                {!this.props.restaurant &&
+                    <p>Fetching this restaurant data...</p>}
 
                 {this.props.restaurant && (
                     <Container>
                         <Row>
                             <Col xs='6' >
                                 <div className="restaurant-img">
-                                    <img src={this.props.restaurant.image_url} alt="restaurant"/>
+                                    <img src={this.props.restaurant.image_url} alt="restaurant" />
                                 </div>
                             </Col>
                             <Col xs='6'>
@@ -66,23 +66,58 @@ class Restaurant extends React.Component {
                                     </Col>
                                 </Row>
                                 <Row>
+                                    <Col >
+                                        <p>
+                                            takeout: {this.props.restaurant.takeout}
+                                        </p>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col >
+                                        <p>
+                                            delivery: {this.props.restaurant.delivery}
+                                        </p>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col >
+                                        <p>
+                                            opens at: {this.props.restaurant.openHour} AM
+                                        </p>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col >
+                                        <p>
+                                            closes at: {this.props.restaurant.closeHour} PM
+                                        </p>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col >
+                                        <p>
+                                            from Mon-Sat
+                                        </p>
+                                    </Col>
+                                </Row>
+                                <Row>
                                     <Col className='visit'>
                                         <span>visited:</span>
                                         <div>
                                             {(this.props.restaurants.find(restaurant =>
                                                 restaurant.id === this.props.restaurant.id)).visited}
                                         </div>
-                                        <button onClick={()=>this.handleClickPlus(this.props.restaurant.id)}>+</button> 
-                                        <button onClick={()=>this.handleClickMinus(this.props.restaurant.id)}>-</button>
+                                        <button onClick={() => this.handleClickPlus(this.props.restaurant.id)}>+</button>
+                                        <button onClick={() => this.handleClickMinus(this.props.restaurant.id)}>-</button>
                                     </Col>
                                 </Row>
                             </Col>
                         </Row>
                     </Container>
-                    
+
                 )}
-                
-                
+
+
             </div>
         )
     }
@@ -92,10 +127,12 @@ class Restaurant extends React.Component {
 const mapStateToProps = (state) => {
 
     console.log(state.restaurants);
-    return {restaurants: state.restaurants.restaurants,
-    restaurant: state.restaurant.restaurant,
-    visited: state.restaurant.visited}
+    return {
+        restaurants: state.restaurants.restaurants,
+        restaurant: state.restaurant.restaurant,
+        visited: state.restaurant.visited
     }
+}
 
 
-export default withRouter(connect(mapStateToProps, {getRestaurantAction, incrementVisits, decrementVisits})(Restaurant));
+export default withRouter(connect(mapStateToProps, { getRestaurantAction, incrementVisits, decrementVisits })(Restaurant));
