07f824fc5c760eb71564d080f2fbf6571eb6e16b
Thierry Joux <tjoux@hotmail.com>
2019-09-27 16:41:13 -0700

    added redirect_ file

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -48,49 +48,47 @@ class Restaurants extends React.Component {
         return (
             <div className='restaurants' >
                 {this.props.restaurants.map( restaurant => 
-                <Container key={restaurant.id}>
-                    <Row className='restaurant'>
-                        <Col xs='6' >
-                            <div className="restaurant-img" 
-                                onClick={()=>this.handleClickImage(restaurant.id)}>
-                                <img src={restaurant.image_url} alt="restaurant" />
-                            </div>
-                        </Col>
-                        <Col xs='6'>
-                            <Row>
-                                <Col className='name' onClick={()=>this.handleClickImage(restaurant.id)}>
-                                    <h2>{restaurant.name}</h2>
-                                </Col>
-                                <Col className='address'>
-                                    <p>{restaurant.address}</p>
-                                    <p>{restaurant.city}</p>
-                                    <p>{restaurant.state}</p>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col >
-                                    <p className='description'>
-                                        {restaurant.description}
-                                    </p>
-                                </Col>
-                            </Row>
-                            <Row className='last-row'>
-                                <Col xs='6'>
-                                    <span>{`visited: ${restaurant.visited}`}</span>
-                                </Col>
-                                <Col xs='6'>
-                                    <Button color='danger' onClick={()=>
-                                        this.handleClickDelete(restaurant.id)}>
-                                        Remove
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                    
+                    <Container key={restaurant.id}>
+                        <Row className='restaurant'>
+                            <Col xs='6' >
+                                <div className="restaurant-img" 
+                                    onClick={()=>this.handleClickImage(restaurant.id)}>
+                                    <img src={restaurant.image_url} alt="restaurant" />
+                                </div>
+                            </Col>
+                            <Col xs='6'>
+                                <Row>
+                                    <Col className='name' onClick={()=>this.handleClickImage(restaurant.id)}>
+                                        <h2>{restaurant.name}</h2>
+                                    </Col>
+                                    <Col className='address'>
+                                        <p>{restaurant.address}</p>
+                                        <p>{restaurant.city}</p>
+                                        <p>{restaurant.state}</p>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col >
+                                        <p className='description'>
+                                            {restaurant.description}
+                                        </p>
+                                    </Col>
+                                </Row>
+                                <Row className='last-row'>
+                                    <Col xs='6'>
+                                        <span>{`visited: ${restaurant.visited}`}</span>
+                                    </Col>
+                                    <Col xs='6'>
+                                        <Button color='danger' onClick={()=>
+                                            this.handleClickDelete(restaurant.id)}>
+                                            Remove
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
                         
-                    
-                </Container>
+                    </Container>
                 )}
                 
             </div>
